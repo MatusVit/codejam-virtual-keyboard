d90e412512d49af9e9a74dd7a59ddc0e59f55d8e
MatusVit <matvi@mail.ru>
2019-11-05 18:51:41 +0300

    feat: add typing of virtual keyboard

--- script.js
+++ script.js
@@ -30,16 +30,18 @@ const KeyBoard = {
       'Tab': 'Tab', 'KeyQ': 'q', 'KeyW': 'w', 'KeyE': 'e', 'KeyR': 'r', 'KeyT': 't', 'KeyY': 'y', 'KeyU': 'u', 'KeyI': 'i', 'KeyO': 'o', 'KeyP': 'p', 'BracketLeft': '[', 'BracketRight': ']', 'Backslash': '\\', 'Delete': 'del',
       'CapsLock': 'CapsLock', 'KeyA': 'a', 'KeyS': 's', 'KeyD': 'd', 'KeyF': 'f', 'KeyG': 'g', 'KeyH': 'h', 'KeyJ': 'j', 'KeyK': 'k', 'KeyL': 'l', 'Semicolon': ';', 'Quote': '\'', 'Enter': 'Enter',
       'ShiftLeft': 'Shift', 'KeyZ': 'z', 'KeyX': 'x', 'KeyC': 'c', 'KeyV': 'v', 'KeyB': 'b', 'KeyN': 'n', 'KeyM': 'm', 'Comma': ',', 'Period': '.', 'Slash': '/', 'ShiftRight': 'Shift',
-      'ControlLeft': 'Ctrl', 'win': 'win', 'AltLeft': 'Alt', 'Space': '', 'AltRight': 'Alt', 'win': 'win', 'ControlRight': 'Ctrl',
+      'ControlLeft': 'Ctrl', 'win': 'win', 'AltLeft': 'Alt', 'Space': ' ', 'AltRight': 'Alt', 'win': 'win', 'ControlRight': 'Ctrl',
     },
     ru: {
       'Backquote': 'ё', 'Digit1': '1', 'Digit2': '2', 'Digit3': '3', 'Digit4': '4', 'Digit5': '5', 'Digit6': '6', 'Digit7': '7', 'Digit8': '8', 'Digit9': '9', 'Digit0': '0', 'Minus': '-', 'Equal': '=', 'BackSpace': 'BackSpace',
       'Tab': 'Tab', 'KeyQ': 'й', 'KeyW': 'ц', 'KeyE': 'у', 'KeyR': 'к', 'KeyT': 'е', 'KeyY': 'н', 'KeyU': 'г', 'KeyI': 'ш', 'KeyO': 'щ', 'KeyP': 'з', 'BracketLeft': 'х', 'BracketRight': 'ъ', 'Backslash': '\\', 'Delete': 'del',
       'CapsLock': 'CapsLock', 'KeyA': 'ф', 'KeyS': 'ы', 'KeyD': 'в', 'KeyF': 'а', 'KeyG': 'п', 'KeyH': 'р', 'KeyJ': 'о', 'KeyK': 'л', 'KeyL': 'д', 'Semicolon': 'ж', 'Quote': 'э', 'Enter': 'Enter',
       'ShiftLeft': 'Shift', 'KeyZ': 'я', 'KeyX': 'ч', 'KeyC': 'с', 'KeyV': 'м', 'KeyB': 'и', 'KeyN': 'т', 'KeyM': 'ь', 'Comma': 'б', 'Period': 'ю', 'Slash': '.', 'ShiftRight': 'Shift',
-      'ControlLeft': 'Ctrl', 'win': 'win', 'AltLeft': 'Alt', 'Space': '', 'AltRight': 'Alt', 'win': 'win', 'ControlRight': 'Ctrl',
+      'ControlLeft': 'Ctrl', 'win': 'win', 'AltLeft': 'Alt', 'Space': ' ', 'AltRight': 'Alt', 'win': 'win', 'ControlRight': 'Ctrl',
     },
   },
+
+
 
   // method for creating new elements
   createNewElement(tag, tagClass, tagText) {
@@ -51,6 +53,71 @@ const KeyBoard = {
 
 
 
+  // method for create KexBox element, arguments:(array keys, array function keys)
+  createKeyBox(arrKeys, arrFuncKeys) {
+    const keyBox = this.createNewElement('div', 'keyboard', '');
+
+    for (let i = 0; i < arrKeys.length; i++) {
+      const keysRow = this.createNewElement('div', 'keys-row', '');
+
+      for (let j = 0; j < arrKeys[i].length; j++) {
+        const key = arrKeys[i][j];
+        let classesStr = `key ${key}`.toLowerCase();
+
+        // add .key--function for function keys
+        if (arrFuncKeys.includes(key)) classesStr = `${classesStr} key--function`;
+
+        const keyElm = this.createNewElement('button', classesStr, '');
+        keyElm.setAttribute('data-key-code', key);
+        keysRow.append(keyElm);
+      }
+      keyBox.append(keysRow);
+    }
+
+    return keyBox;
+  },
+
+
+
+  // method for make keyboard language layout, argument:('en' or 'ru')
+  setLanguageLayout(languageToggle) {
+    const arrKeys = this.elements.keyBox.querySelectorAll('.key');
+
+    for (let elem of arrKeys) {
+      const keySymbol = this.language[languageToggle][elem.dataset.keyCode];
+      elem.innerHTML = keySymbol;
+    }
+
+    this.inputAria.languageToggle = languageToggle;
+  },
+
+
+  // method switch Language Layout for keyboard, argument:(not)
+  switchLanguageLayout() {
+    if (this.inputAria.languageToggle === 'en') {
+      this.inputAria.languageToggle = 'ru';
+    } else if (this.inputAria.languageToggle === 'ru') {
+      this.inputAria.languageToggle = 'en';
+    }
+    this.setLanguageLayout(this.inputAria.languageToggle);
+  },
+
+  //method type from screen keys
+  type(scr) {
+  // debugger;    
+    let text = this.elements.textAria.value;
+    let st = this.elements.textAria.selectionStart;
+    let end = this.elements.textAria.selectionEnd;
+
+    this.elements.textAria.value = text.substring(0, st) + scr + text.substring(end);
+    this.elements.textAria.focus();
+    this.elements.textAria.selectionStart = this.elements.textAria.selectionEnd = st + 1;
+  },
+
+
+
+
+  // initialization object KeyBoard
   init() {
     // add title
     this.elements.title = this.createNewElement('h1', 'title', 'Virtual KeyBoard');
@@ -62,28 +129,11 @@ const KeyBoard = {
     this.elements.textAria.value = 'enter text here'
 
     // add keyBox with keys
-    this.elements.keyBox = this.createNewElement('div', 'keyboard', '');
-
-    for (let i = 0; i < this.elements.keys.length; i++) {
-      const keysRow = this.createNewElement('div', 'keys-row', '');
-
-      for (let j = 0; j < this.elements.keys[i].length; j++) {
-        const key = this.elements.keys[i][j];
-        let classesStr = `key ${key}`.toLowerCase();
-
-        // add .key--function for function keys
-        if (this.elements.functionKeys.includes(key)) classesStr = `${classesStr} key--function`;
-
-        const keySymbol = this.language[this.inputAria.languageToggle][key];
-        const keyElm = this.createNewElement('button', classesStr, keySymbol);
-        keysRow.append(keyElm);
-      }
-      this.elements.keyBox.append(keysRow);
-    }
-
+    this.elements.keyBox = this.createKeyBox(this.elements.keys, this.elements.functionKeys);
+    this.setLanguageLayout(this.inputAria.languageToggle);
     document.body.append(this.elements.keyBox);
 
-    // EventListener keydown
+    // add EventListener keydown
     window.addEventListener("keydown", (event) => {
       let selector = `.key.${event.code.toLowerCase()}`;
       let elm = this.elements.keyBox.querySelector(selector);
@@ -99,6 +149,19 @@ const KeyBoard = {
       elm.classList.remove('active');
     });
 
+    // EventListener keyBox
+    this.elements.keyBox.addEventListener("click", (event) => {
+      let target = event.target;
+
+      if (target.classList.contains('key')) {
+          let char = target.textContent;
+          this.type(char);
+          return;
+        }
+    });
+
+
+    // this.switchLanguageLayout();
   },
 
 
@@ -115,6 +178,12 @@ const KeyBoard = {
   // },
 };
 
+
+
 window.addEventListener('DOMContentLoaded', () => {
   KeyBoard.init();
 })
+
+
+// todo
+// 
